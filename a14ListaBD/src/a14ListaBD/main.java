abe8631f94aba2d84218d81daccc7434aaa9e10a
manuel <mmf19500>
2022-04-05 14:01:52 +0200

    g

--- a14ListaBD/src/a14ListaBD/main.java
+++ a14ListaBD/src/a14ListaBD/main.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
+import javax.sql.RowSetInternal;
+
 public class main {
 
 	static Scanner sc=new Scanner(System.in);
@@ -70,14 +72,111 @@ public class main {
 					res.next();
 					
 //																		recoje la PK obtenida
-					System.out.println("Categoría insetada con el ID: "+res.getInt(1));
+					System.out.println("Producto insetado con el ID: "+res.getInt(1));
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			
 			break;
 			
-			
+			case "2":
+//				hacer tmbn por nombre
+				System.out.println("1) Modificar nombre \n2) Modificar cantidad");
+
+				if (Integer.parseInt(sc.nextLine())==1) {
+//					modificar  nombre
+					System.out.println("ID del producto a modificar:");
+					int id=Integer.parseInt(sc.nextLine());
+					
+					System.out.println("Nombre del nuevo producto");
+					String nombre=sc.nextLine();
+					stm=null;
+					
+					String update= "update productos set producto=? where id=?";
+				
+					try {
+						stm=con.prepareStatement(update);
+						
+						stm.setString(1, nombre);
+						stm.setInt(2, id);
+						
+						stm.executeUpdate();
+						
+						System.out.println("Datos actualizados");
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				
+				} else {
+//					modificar cantidad
+					System.out.println("ID del producto:");
+					int id=Integer.parseInt(sc.nextLine());
+					
+					System.out.println("Nueva cantidad");
+					can=Integer.parseInt(sc.nextLine());
+					stm=null;
+					
+					String update= "update productos set cantidad=? where id=?";
+				
+					try {
+						stm=con.prepareStatement(update);
+						
+						stm.setInt(1, can);
+						stm.setInt(2, id);
+						
+						stm.executeUpdate();
+						
+						System.out.println("Datos actualizados");
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				break;
+				
+			case "3":
+				
+				System.out.println("Id del producto a eliminar");
+				int ide=Integer.parseInt(sc.nextLine());
+				
+//				para acordarme
+				stm=null;
+				res=null;
+				
+				String delete="delete from productos where producto=?";
+				
+				try {
+					stm=con.prepareStatement(delete);
+					stm.setInt(1, ide);
+					stm.executeUpdate();
+					
+					System.out.println("Datos actualizados");
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				
+				break;
+				
+			case "4":
+				
+				stm=null;
+				res=null;
+				
+				String consulta="Select * from productos order by producto desc";
+				
+				try {       
+					stm=con.prepareStatement(consulta);
+					res=stm.executeQuery();
+					
+					while (res.next()) {               // muestras solo el titulo puedes ponerlo en el select
+						System.out.println(res.getString("producto")+" - "+(res.getInt("cantidad")));
+					}
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				
+				break;
 		}
 	}
 
